aa80858fd1a3b41f1679e50989e711d0a8525630
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2024-07-28 18:28:11 +0530

    Add files via upload

---
+++ FUNCTIONS/Function1.java
@@ -0,0 +1,14 @@
+class Function1{
+    public static String fullName(String x,String y){
+        String sum=x+" "+y;
+        return sum;
+    }
+
+
+   public static void main(String[] args) {
+    String a="Gopal";
+    String b="Dhangar";
+    String ans=fullName(a, b);
+    System.out.println(ans);
+   } 
+}
