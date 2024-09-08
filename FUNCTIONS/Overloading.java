6ac8845f69d96a1e0c0d58f660201636f12c34e3
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2024-09-08 19:49:15 +0530

    Add files via upload

---
+++ FUNCTIONS/Overloading.java
@@ -0,0 +1,18 @@
+public class Solution {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(5, 2));
+        System.out.println(sum(3, 4, 5));
+
+    }
+
+}
