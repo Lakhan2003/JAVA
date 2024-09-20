6a8b5f5959e7a816654159d5fbbd7788f8aaa51d
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2024-09-20 10:17:50 +0530

    Update Prime.java

--- FUNCTIONS/Prime.java
+++ FUNCTIONS/Prime.java
@@ -1,22 +1,16 @@
-import java.util.*;
+ import java.util.*;
 
 public class Prime {
-    public static String isPrime(int n) {
-        String a = "notprime";
-        String b = "prime";
+    public static boolean isPrime(int n) {
         for (int i = 2; i < n; i++) {
-            if (n % i == 0)
-                ;
-            return a;
+            if (n % i == 0) {
+                return false;
+            }
         }
-        return b  ;
+        return true;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(" enter no.");
-        int n = sc.nextInt();
-        System.out.println(isPrime(n));
+        System.out.println(isPrime(12));
     }
-
 }
