3b5c6690d737714121f26e85ad7892c135ece360
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2024-10-13 13:46:06 +0530

    Update SelectionSort.java

--- ARRAYS/SelectionSort.java
+++ ARRAYS/SelectionSort.java
@@ -1,26 +1,25 @@
-
 public class SelectionSort {
     public static void selectionSort(int nums[]) {
         for (int i = 0; i < nums.length; i++) {
             int minpos = i;
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[minpos] > nums[j]) {
-                    minpos = j ;
+                    minpos = j;
                 }
             }
             int temp = nums[minpos];
-             nums[minpos] = nums[i];
-             nums[i]=temp;
+            nums[minpos] = nums[i];
+            nums[i] = temp;
 
         }
-       
+
     }
 
     public static void main(String[] args) {
         int nums[] = { 3, 5, 2, 4, 1 };
         selectionSort(nums);
         for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]+",");
+            System.out.print(nums[i] + ",");
 
         }
     }
