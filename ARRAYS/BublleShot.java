a3629e6da4e09dbcb82b493ce574bc8c8b604463
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2024-10-13 11:16:02 +0530

    Add files via upload

---
+++ ARRAYS/BublleShot.java
@@ -0,0 +1,23 @@
+public class BublleShot {
+    public static void bubbleShot(int arr[]) {
+
+        for (int turns = 0; turns < arr.length - 1; turns++) {
+            for (int j = 0; j < arr.length - 1 - turns; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 3, 1, 2, 4 };
+        bubbleShot(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
