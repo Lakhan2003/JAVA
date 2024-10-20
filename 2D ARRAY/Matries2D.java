508ea8a591bcec06e56aa979960b7b0f138b7b9f
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2024-10-20 13:20:33 +0530

    Add files via upload

---
+++ 2D ARRAY/Matries2D.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Matries2D {
+    public static void search(int matrix[][], int key) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == key) {
+                    System.out.println("found at cell ( " + i + "," + j + " )");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m = matrix[0].length;
+
+        // input of an 2d matrix
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" write an 2d matrix numbers ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        // output of an 2d matrix
+        System.out.println(" here are our matrix ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        search(matrix, 5);
+    }
+}
