2c42f1eb334589a7bfd69f2936e9678f429acd76
RamLakhan Dhangar <138513313+Lakhan2003@users.noreply.github.com>
2024-11-26 21:58:09 +0530

    Add files via upload

---
+++ STRING/largesgtString.java
@@ -0,0 +1,13 @@
+public class largesgtString {
+    public static void main(String[] args) {
+        String fruits[] = { "apple", "banana", "mango", "tomato" };
+        String largest = fruits[0];
+        for(int i = 0; i < fruits.length; i++) {
+            if (largest.compareTo(fruits[i]) < 0) {
+                largest = fruits[i];
+
+            }
+        }
+        System.out.println(largest);
+    }
+}
